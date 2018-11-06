5baf698d52c64c3895eed587a7e22ce511c64d64
Daniel Chamorro <dan@dcitllc.com>
2018-11-06 17:52:40 -0500

    MongoDB sucessfully connected

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://auto-invoice:XheKLVXlOIQF0bSk@auto-invoicer-angvy.mongodb.net/test?retryWrites=true"
+};
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const routeConfig = require("./config/routes");
 const moduleConfig = require("./config/modules");
 const serveClient = require("./config/serveClient");
 
 const server = express();
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 moduleConfig(server);
 routeConfig(server);
