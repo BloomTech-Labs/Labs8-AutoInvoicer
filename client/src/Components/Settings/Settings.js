be4202b1b14062cf4cb789b1a2dbe33d07118af7
markdavidwilliams <markwilliams.aus@gmail.com>
2018-11-06 17:25:18 -0600

    Settings Component Built out form Created form change handler Created submit handler, with TODO implement axios later

---
+++ client/src/Components/Settings/Settings.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './Settings.css';
+
+class Settings extends Component {
+  state = {
+    email: '',
+    oldPassword: '',
+    newPassword: ''
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // TODO: axios request
+  }
+
+  render() {
+    return (
+      <div className="settings-wrapper">
+        <form>
+          <label>
+            Email:
+            <input name="email" type="email" onChange={this.handleChange} />
+          </label>
+          <label>
+            Old Password:
+            <input name="oldPassword" type="password" onChange={this.handleChange} />
+          </label>
+          <label>
+            New Password:
+            <input name="newPassword" type="password" onChange={this.handleChange} />
+          </label>
+          <button type="submit" onClick={this.handleSubmit}>Save</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Settings
