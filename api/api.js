111d97417b3c4dff5d946a70f4d8a11060ef8dc5
markdavidwilliams <markwilliams.aus@gmail.com>
2018-11-06 14:49:16 -0600

    api routes scaffolded api routes structure laid out endpoints to be built out later

--- api/api.js
+++ api/api.js
@@ -1,6 +1,14 @@
-const api = require('express').Router()
-const test = require('./test')
+const api = require("express").Router();
+const test = require("./test");
+const login = require("./routes/login");
+const register = require("./routes/register");
+const users = require("./routes/users");
+const invoices = require("./routes/invoices");
 
-api.use('/test', test)
+api.use("/test", test);
+api.use("/login", login);
+api.use("/register", register);
+api.use("/users", users);
+api.use("/invoices", invoices);
 
-module.exports = api
+module.exports = api;
