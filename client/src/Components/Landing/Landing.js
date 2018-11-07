7e4e116989f565f54ebc273803e3c5fd47f889ee
Kevin Tena <kevten09@gmail.com>
2018-11-06 18:15:19 -0600

    Ran code through Prettier and changed state config

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import "./Landing.css";
 
 export default class LandingPage extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
+    state = {};
 
     render() {
         return <div className="landing-page-container">
-                    <div className="login-bar-container">
-                        <a>Sign Up</a>
-                        <a>Sign In</a>
-                    </div>
-                    <div className="centerpiece-container">
-                        <h3>Auto-invoicer helps small businesses generate high-quality PDF invoices and track them.</h3>
-                        <button className="buy-button">BUY NOW</button>
-                    </div>
-                </div>
+            <div className="login-bar-container">
+                <a>Sign Up</a>
+                <a>Sign In</a>
+            </div>
+            <div className="centerpiece-container">
+                <h3>Auto-invoicer helps small businesses generate high-quality PDF invoices and track them.</h3>
+                <button className="buy-button">BUY NOW</button>
+            </div>
+        </div>
     }
 }
