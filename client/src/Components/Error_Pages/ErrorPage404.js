59991aa1a16225c5b5256c0520c8a48f48479cc6
Mark Hong <m4rkh0ng@gmail.com>
2018-11-07 17:59:54 -0500

    added error pages for 401 and 404 responses

---
+++ client/src/Components/Error_Pages/ErrorPage404.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Error401 extends Component {
+    render() {
+        return (
+        <div className="error-page">
+            404 — Bad Request
+        </div>
+        )
+    }
+}
