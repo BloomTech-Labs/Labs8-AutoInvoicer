5b2636fffd5390ac290f7351c6a265b570a55b2f
markdavidwilliams <markwilliams.aus@gmail.com>
2018-11-06 19:46:48 -0600

    refactor abstracted mongo connect to config/mongo.js placed mongoURI in secret env variable placed registration api in api/register.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,28 +1,7 @@
 const router = require("express").Router();
 
-// Load User model
-const User = require("../../models/User");
-
-// @route GET api/users/test
-// @desc Tests users route
-// @access Public
-router.get("/test", (req, res) => res.json({ msg: "User Works" }));
-
-router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      return res.status(400).json({ email: "Email already exists" });
-    } else {
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      })
-        .save()
-        .then(user => res.json(user))
-        .catch(err => console.log(err));
-    }
-  });
+router.get("/", (req, res) => {
+  // possibly may need to be a post
 });
 
 router.put("/", (req, res) => {
--- config/modules.js
+++ config/modules.js
@@ -1,9 +1,10 @@
-const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 module.exports = server => {
   server.use(helmet());
   server.use(cors());
-  server.use(express.json());
+  server.use(bodyParser.urlencoded({ extended: false }));
+  server.use(bodyParser.json());
 };
