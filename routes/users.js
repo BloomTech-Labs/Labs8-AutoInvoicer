b1a67d78b533e261edb36b5d292f07353db7f131
Mark Hong <m4rkh0ng@gmail.com>
2018-11-07 14:40:31 -0500

    prettified files and cleared merge conflicts

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var secured = require('../lib/middleware/secured');
+var express = require("express");
+var secured = require("../lib/middleware/secured");
 var router = express.Router();
 
 /* GET user profile. */
-router.get('/user', secured(), function (req, res, next) {
+router.get("/user", secured(), function(req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
-  res.render('user', {
+  res.render("user", {
     userProfile: JSON.stringify(userProfile, null, 2),
-    title: 'Profile page'
+    title: "Profile page"
   });
 });
 
--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require("express");
-const path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var dotenv = require('dotenv');
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
-var flash = require('connect-flash');
-var userInViews = require('./lib/middleware/userInViews');
-var authRouter = require('./routes/auth');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var dotenv = require("dotenv");
+var passport = require("passport");
+var Auth0Strategy = require("passport-auth0");
+var flash = require("connect-flash");
+var userInViews = require("./lib/middleware/userInViews");
+var authRouter = require("./routes/auth");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-<<<<<<< HEAD
-const connectMongo = require("./config/mongo")
+const connectMongo = require("./config/mongo");
 const routeConfig = require("./config/routes");
-=======
 dotenv.load();
 
-// const routeConfig = require("./config/routes");
->>>>>>> completed login test for auth0 on the backend
 const moduleConfig = require("./config/modules");
 const serveClient = require("./config/serveClient");
 
@@ -30,9 +26,9 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:8000/callback'
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:8000/callback"
   },
-  function (accessToken, refreshToken, extraParams, profile, done) {
+  function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
@@ -43,35 +39,33 @@ var strategy = new Auth0Strategy(
 passport.use(strategy);
 
 // You can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
+passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
 const server = express();
 
-<<<<<<< HEAD
 connectMongo(server);
-=======
 // View engine setup
-server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', 'pug');
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "pug");
 
-server.use(logger('dev'));
+server.use(logger("dev"));
 server.use(cookieParser());
 
 // config express-session
 var sess = {
-  secret: 'secretsecretsaresecrets',
+  secret: "secretsecretsaresecrets",
   cookie: {},
   resave: false,
   saveUninitialized: true
 };
 
-if (server.get('env') === 'production') {
+if (server.get("env") === "production") {
   sess.cookie.secure = true; // serve secure cookies, requires https
 }
 
@@ -79,29 +73,29 @@ server.use(session(sess));
 
 server.use(passport.initialize());
 server.use(passport.session());
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 
 server.use(flash());
 
 // Handle auth failure error messages
-server.use(function (req, res, next) {
+server.use(function(req, res, next) {
   if (req && req.query && req.query.error) {
-    req.flash('error', req.query.error);
+    req.flash("error", req.query.error);
   }
   if (req && req.query && req.query.error_description) {
-    req.flash('error_description', req.query.error_description);
+    req.flash("error_description", req.query.error_description);
   }
   next();
 });
 
 server.use(userInViews());
-server.use('/', authRouter);
-server.use('/', indexRouter);
-server.use('/', usersRouter);
+server.use("/", authRouter);
+server.use("/", indexRouter);
+server.use("/", usersRouter);
 
 // Catch 404 and forward to error handler
-server.use(function (req, res, next) {
-  const err = new Error('Not Found');
+server.use(function(req, res, next) {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -110,10 +104,10 @@ server.use(function (req, res, next) {
 
 // Development error handler
 // Will print stacktrace
-if (server.get('env') === 'development') {
-  server.use(function (err, req, res, next) {
+if (server.get("env") === "development") {
+  server.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -122,16 +116,15 @@ if (server.get('env') === 'development') {
 
 // Production error handler
 // No stacktraces leaked to user
-server.use(function (err, req, res, next) {
+server.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
 });
 
-// routeConfig(server);
->>>>>>> completed login test for auth0 on the backend
+routeConfig(server);
 moduleConfig(server);
 serveClient(server);
 
