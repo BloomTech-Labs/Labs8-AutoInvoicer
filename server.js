e0a867b4138ceba3416021401748b52226fd579b
Mark Hermansen <markwilliams.aus@gmail.com>
2018-11-06 20:03:04 -0600

    abstracted mongo connect to config/mongo.js

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
+const connectMongo = require("./config/mongo")
 const routeConfig = require("./config/routes");
 const moduleConfig = require("./config/modules");
 const serveClient = require("./config/serveClient");
 
-// Routes
-const users = require("./api/routes/users");
-
 const server = express();
 
-// Body parser middleware
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json());
-
-// DB Config
-const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-// Home test route
-server.get("/", (req, res) => res.send("Home"));
-
-// Use routes
-server.use("/api/users", users);
-
+connectMongo(server);
 moduleConfig(server);
 routeConfig(server);
 serveClient(server);
