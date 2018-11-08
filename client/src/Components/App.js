ddbf96eaa7df10e4f1946ada19f54e3d620d2767
Daniel Chamorro <dan@dcitllc.com>
2018-11-08 13:49:22 -0500

    Added router to /settings /register and /landing(Home Page). Tested routes

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Import pages
+import LandingPage from "./Landing/Landing";
+import SignUp from "./SignUp/SignUp";
+import Settings from "./Settings/Settings";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, world!</h1>
-        <h1>Auto-Invoicer</h1>
-      </div>
+      <Router>
+        <div className="App">
+          {/* <h1>Hello, world!</h1>
+          <h1>Auto-Invoicer</h1> */}
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/register" component={SignUp} />
+            <Route path="/settings" component={Settings} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
