89541e2d5a42a0bd5a1375be3bcf4b50c8c1d7aa
maribelcuales <maribelcuales@gmail.com>
2018-11-09 16:12:28 +0800

    ran prettier on AddInvoice.js and AddInvoice.css

--- client/src/Components/AddInvoice/AddInvoice.js
+++ client/src/Components/AddInvoice/AddInvoice.js
@@ -5,16 +5,16 @@ import { NavLink, Link, Route } from "react-router-dom";
 import "./AddInvoice.css";
 
 class AddInvoice extends Component {
-    render() {
-        return (
-            <div className="newInvoice">
-              <h2 >Add a New Invoice</h2>
-              <Link to="/new" exact>
-                  Add Here
-              </Link>
-            </ div>
-        );
-    }
+  render() {
+    return (
+      <div className="newInvoice">
+        <h2>Add a New Invoice</h2>
+        <Link to="/new" exact>
+          Add Here
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default AddInvoice;
