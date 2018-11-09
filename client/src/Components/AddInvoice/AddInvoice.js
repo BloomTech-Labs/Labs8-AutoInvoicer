9d7d958231cb14fcc27e205d5babba6208f841d3
maribelcuales <maribelcuales@gmail.com>
2018-11-09 23:57:36 +0800

    separated the Navbar component into Navbar(left side nav) and TopNav(top menu), created TopNav folder and files (JS and CSS), separated code for TopNav component from Navbar and transfer to the new files

--- client/src/Components/AddInvoice/AddInvoice.js
+++ client/src/Components/AddInvoice/AddInvoice.js
@@ -3,15 +3,22 @@ import { NavLink, Link, Route } from "react-router-dom";
 
 // import App from "../App.js";
 import "./AddInvoice.css";
+import Navbar from "../Navbar/Navbar";
+import TopNav from "../TopNav/TopNav";
+
 
 class AddInvoice extends Component {
   render() {
     return (
-      <div className="newInvoice">
-        <h2>Add a New Invoice</h2>
-        <Link to="/new" exact>
-          Add Here
-        </Link>
+      <div>
+        <TopNav />
+        <Navbar />
+        <div className="newInvoice">
+          <h2>Add a New Invoice</h2>
+          <Link to="/new" exact>
+            Add Here
+          </Link>
+        </div>
       </div>
     );
   }
--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -3,45 +3,25 @@ import { NavLink, Link, Route } from "react-router-dom";
 
 // import App from "../App.js";
 import "./Navbar.css";
-import AddInvoice from "../AddInvoice/AddInvoice";
 
 class Navbar extends Component {
   render() {
     return (
       <div className="NavBar">
         <div className="NavBar-header">
-          <nav>
-            <div className="top-nav">
-              <NavLink className="home" to="/" exact>
-                Home
-              </NavLink>
-              &nbsp; > &nbsp;
-              <NavLink className="invoices1" to="/invoices" exact>
-                Invoices
-              </NavLink>
-            </div>
-            <div className="signout-container">
-              <NavLink className="signout" to="/signout" exact>
-                Signout
-              </NavLink>
-            </div>
-          </nav>
-            <div>
-              <AddInvoice />
-            </div>
-            <div class="side-nav">
-              <NavLink className="invoices2" to="/invoices" exact>
-                Invoices
-              </NavLink>
-              <NavLink className="billing" to="/billing" exact>
-                Billing
-              </NavLink>
-              <NavLink className="settings" to="/settings" exact>
-                Settings
-              </NavLink>
-            </div>
+          <div class="side-nav">
+            <NavLink className="invoices2" to="/invoices" exact>
+              Invoices
+            </NavLink>
+            <NavLink className="billing" to="/billing" exact>
+              Billing
+            </NavLink>
+            <NavLink className="settings" to="/settings" exact>
+              Settings
+            </NavLink>
           </div>
         </div>
+      </div>
     );
   }
   signout = () => {
