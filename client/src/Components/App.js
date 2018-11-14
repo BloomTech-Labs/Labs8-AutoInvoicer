110591fb7f9e654e87b3bcc16f4c91f26c098ff2
Daniel Chamorro <dan@dcitllc.com>
2018-11-13 20:36:19 -0500

    Added Stripe to front end

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -6,6 +6,9 @@ import LandingPage from "./Landing/Landing";
 import SignUp from "./SignUp/SignUp";
 import Settings from "./Settings/Settings";
 
+// Import Stripe Checkout
+import Checkout from "../Components/Stripe/Checkout";
+
 import "./App.css";
 
 class App extends Component {
@@ -20,6 +23,11 @@ class App extends Component {
             <Route path="/register" component={SignUp} />
             <Route path="/settings" component={Settings} />
           </Switch>
+          <Checkout
+            name={"Auto Invoicer"}
+            description={"#1 Invoicing App"}
+            amount={1}
+          />
         </div>
       </Router>
     );
