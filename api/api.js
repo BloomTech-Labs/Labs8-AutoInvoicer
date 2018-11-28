cb7dcd3ce8e82ca972f83f5e14de47339161f056
Mark Hong <m4rkh0ng@gmail.com>
2018-11-28 14:40:12 -0500

    api.js fix

--- api/api.js
+++ api/api.js
@@ -1,12 +1,8 @@
 const api = require("express").Router();
-const login = require("./routes/login");
-const register = require("./routes/register");
 const users = require("./routes/users");
 const invoices = require("./routes/invoices");
 const charge = require("./routes/charge");
 
-api.use("/login", login);
-api.use("/register", register);
 api.use("/users", users);
 api.use("/invoices", invoices);
 api.use("/charge", charge);
