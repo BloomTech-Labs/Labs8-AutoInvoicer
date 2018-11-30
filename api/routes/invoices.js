e6397b8bc410c3ed706390d1c88b69795fc3c8a6
Kevin Tena <kevten09@gmail.com>
2018-11-30 09:59:23 -0600

    Added logic to convert InvoiceForm to edit mode and update endpoint to reflect changes

--- api/routes/invoices.js
+++ api/routes/invoices.js
@@ -117,16 +117,51 @@ router.post("/", upload.single('logo'), (req, res) => {
 
 });
 
-router.put("/api/invoices/:_id", (req, res) => {
+router.put("/api/invoices/:_id", upload.single('logo'), (req, res) => {
   // for editing existing invoices
-  let edit = req.body || {},
-    options = {}
+  let options = {
+      new: true
+    }
 
   const invoice_number = req.params._id;
   let single_invoice;
-
   const { _raw, _json, ...userProfile } = req.user;
   const auth0_userID = req.user._json.sub.split("|")[1];
+  const ext = req.file.originalname.split('.')[1]
+  const tmp = os.tmpdir() + '/deleteme.' + ext
+  fs.writeFileSync(tmp, req.file.buffer)
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+  })
+
+  cloudinary.v2.uploader.upload(tmp, {public_id: `auto-invoicer/${Date.now()}`}, (error, result) => {
+    const edit = {
+      logo: result.secure_url,
+      invoice_number: Number(req.body.invoice_number),
+      date: req.body.date,
+      due_date: req.body.due_date,
+      balance_due: Number(req.body.balance_due),
+      address: req.body.address,
+      zipcode: req.body.zipcode,
+      city: req.body.city,
+      state: req.body.state,
+      company_name: req.body.company_name,
+      item: req.body.item,
+      quantity: Number(req.body.quantity),
+      rate: Number(req.body.rate),
+      amount: Number(req.body.amount),
+      subtotal: Number(req.body.subtotal),
+      discount: Number(req.body.discount),
+      tax: Number(req.body.tax),
+      shipping: Number(req.body.shipping),
+      total: Number(req.body.total),
+      amount_paid: Number(req.body.amount_paid),
+      notes: req.body.notes,
+      terms: req.body.terms
+    };
 
   User.findOne({auth0_userID})
       .populate('invoices')
@@ -135,8 +170,11 @@ router.put("/api/invoices/:_id", (req, res) => {
           if(invoice._id == invoice_number)
             single_invoice = invoice._id;
         })
-       
-        Invoice.findOneAndUpdate({ _id: single_invoice}, edit, options)
+        
+        edit.total = 0;//Update can't occur if the total is null or NaN
+                       //Need to add logic to front end to prevent this
+
+        Invoice.updateOne({ _id: single_invoice}, edit)
           .then(invoice =>{
             res.status(200).send(invoice);
         })
@@ -149,16 +187,7 @@ router.put("/api/invoices/:_id", (req, res) => {
         res.status(500);
         console.log(err, "Unable to look up user for invoice ID.");
       })
-
-    Invoice.findOneAndUpdate({_id: req.params._id}, edit, options)
-    .then(item => {
-      res.send(item)
     })
-    .catch(err => {
-      res.status(500)
-      console.log(err)
-    })
-
 });
 
 router.delete("/:_id", (req, res) => {
--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -85,11 +85,24 @@ class App extends Component {
                   auth0_userID={this.state.user.auth0_userID}
                   invoice_num={this.state.user.invoice_num}
                   mongo_id={this.state.user._id}
+                  path={props.match.path}
+                  params={props.match.params}
                 />
               )}
             />
             <Route key={this.routeKey()} exact path="/" component={Invoices} />
-            <Route key={this.routeKey()} path="/invoices/:id" component={InvoiceForm} />
+            <Route key={this.routeKey()} 
+              path="/invoices/:id" 
+              render={props => (
+                <InvoiceForm
+                  auth0_userID={this.state.user.auth0_userID}
+                  invoice_num={this.state.user.invoice_num}
+                  mongo_id={this.state.user._id}
+                  path={props.match.path}
+                  params={props.match.params}
+                />
+              )}
+            />
             <Route
               key={this.routeKey()}
               path="/empty_invoice"
