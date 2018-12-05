6c05363e42f76731701242580ce2832a48b9e482
Mark Hong <m4rkh0ng@gmail.com>
2018-12-05 18:25:57 -0500

    changed invoice cards to including status circle for paid/unpaid status

--- client/src/Components/Invoices/Invoices.js
+++ client/src/Components/Invoices/Invoices.js
@@ -25,13 +25,17 @@ export default class Invoices extends Component {
         <div className="invoices">
          {this.state.invoices === null && <p>Loading invoices...</p>}
         {this.state.invoices && this.state.invoices.map(invoice => (
-            <div key={invoice._id} className={invoice.balance_due - invoice.amount_paid === 0 ? "invoice paid"  : "invoice unpaid" }>
+            <div key={invoice._id} className="invoice">
+              <div className="status-circle-container">
+                <div className={invoice.balance_due - invoice.amount_paid === 0 ? "status-circle paid"  : "status-circle unpaid" }>
+                </div>
+              </div>
               <Link to={`/invoices/${invoice._id}`}>
-                  <h4>Invoice #{invoice.invoice_number}</h4>
-                  <p> Due Date: {invoice.due_date}</p>
-                  <p> Company: {invoice.company_name}</p>
-                  <p> {invoice.balance_due - invoice.amount_paid === 0 ? "Status: Payment Complete" : `Remaining Balance: $${invoice.balance_due - invoice.amount_paid}`} </p>
-                  <p className="late"> {invoice.due_date < Date.now() && invoice.balance_due - invoice.amount_paid > 0 ? "Status: Overdue" : "" }</p> 
+                <h4>Invoice #{invoice.invoice_number}</h4>
+                <p> Due Date: {invoice.due_date}</p>
+                <p> Company: {invoice.company_name}</p>
+                <p> {invoice.balance_due - invoice.amount_paid === 0 ? "Status: Payment Complete" : `Remaining Balance: $${invoice.balance_due - invoice.amount_paid}`} </p>
+                <p className="late"> {invoice.due_date < Date.now() && invoice.balance_due - invoice.amount_paid > 0 ? "Status: Overdue" : "" }</p> 
               </Link>
             </div>
           ))
