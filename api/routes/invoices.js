8eae849062f06ec63b24fe5869a1a95e67526fc9
Kevin Tena <kevten09@gmail.com>
2018-12-06 16:59:35 -0600

    Prettier formatted

--- api/routes/invoices.js
+++ api/routes/invoices.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
-const multer = require('multer')
-const fs = require('fs')
-const cloudinary = require('cloudinary')
-const os = require('os')
+const multer = require("multer");
+const fs = require("fs");
+const cloudinary = require("cloudinary");
+const os = require("os");
 
 // Load Invoice model
 const Invoice = require("../../models/Invoice");
 const User = require("../../models/User");
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage})
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 //Get the list of all invoices from a user
 router.get("/api/invoices", (req, res) => {
@@ -17,120 +17,127 @@ router.get("/api/invoices", (req, res) => {
   const { _raw, _json, ...userProfile } = req.user;
 
   const auth0_userID = req.user._json.sub.split("|")[1];
-  
+
   User.findOne({ auth0_userID })
-      .populate('invoices')
-      .then(user => {
-        res.status(200).send(user.invoices);
-      })
-      .catch(err => {
-        res.status(500);
-        console.log(err);
-      })
+    .populate("invoices")
+    .then(user => {
+      res.status(200).send(user.invoices);
+    })
+    .catch(err => {
+      res.status(500);
+      console.log(err);
+    });
 });
 
 // NOT FINISHED YET Get a specific invoice from the logged in user by its invoice ID
 router.get("/api/invoices/:_id", (req, res) => {
-
   const invoice_number = req.params._id;
   let single_invoice;
 
   const { _raw, _json, ...userProfile } = req.user;
   const auth0_userID = req.user._json.sub.split("|")[1];
 
-  User.findOne({auth0_userID})
-      .populate('invoices')
-      .then(user => {
-        /* Check to see if the invoice is part of the user's invoice,
+  User.findOne({ auth0_userID })
+    .populate("invoices")
+    .then(user => {
+      /* Check to see if the invoice is part of the user's invoice,
         personally I feel this is redundant but I included it for assurance that the
         invoice was part of the user's invoices -Kevin */
-        user.invoices.forEach( invoice => {
-          if(invoice._id == invoice_number)
-            single_invoice = invoice._id;
-        })
-       
-        Invoice.findOne({ _id: single_invoice})
-          .then(invoice =>{
-            res.status(200).send(invoice);
-        })
-          .catch(err =>{
-            res.status(500);
-            console.log(err, "User look up successful but unable to find invoice with that ID");
-          })
-      })
-      .catch(err => {
-        res.status(500);
-        console.log(err, "Unable to look up user for invoice ID.");
-      })
-})
+      user.invoices.forEach(invoice => {
+        if (invoice._id == invoice_number) single_invoice = invoice._id;
+      });
 
-router.post("/", upload.single('logo'), (req, res) => {
+      Invoice.findOne({ _id: single_invoice })
+        .then(invoice => {
+          res.status(200).send(invoice);
+        })
+        .catch(err => {
+          res.status(500);
+          console.log(
+            err,
+            "User look up successful but unable to find invoice with that ID"
+          );
+        });
+    })
+    .catch(err => {
+      res.status(500);
+      console.log(err, "Unable to look up user for invoice ID.");
+    });
+});
+
+router.post("/", upload.single("logo"), (req, res) => {
   // for creating new invoices
   console.log("File: ", req.file);
   console.log(req.body);
   const auth0_userID = req.body.auth0_userID;
-  const ext = req.file.originalname.split('.')[1]
-  const tmp = os.tmpdir() + '/deleteme.' + ext
-  fs.writeFileSync(tmp, req.file.buffer)
+  const ext = req.file.originalname.split(".")[1];
+  const tmp = os.tmpdir() + "/deleteme." + ext;
+  fs.writeFileSync(tmp, req.file.buffer);
 
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
-  })
+  });
 
   const lineItems = JSON.parse(req.body.lineItems);
 
   lineItems.forEach(row => {
     row.quantity = Number(row.quantity);
     row.rate = Number(row.rate);
-  })
+  });
 
-  cloudinary.v2.uploader.upload(tmp, {public_id: `auto-invoicer/${Date.now()}`}, (error, result) => {
-    const newInvoice = new Invoice({
-      logo: result.secure_url,
-      invoice_number: Number(req.body.invoice_number),
-      date: req.body.date,
-      due_date: req.body.due_date,
-      balance_due: Number(req.body.balance_due),
-      address: req.body.address,
-      zipcode: req.body.zipcode,
-      city: req.body.city,
-      state: req.body.state,
-      company_name: req.body.company_name,
-      // item: req.body.item,
-      // quantity: Number(req.body.quantity),
-      // rate: Number(req.body.rate),
-      invoiceTo: req.body.invoiceTo,
-      line_items: lineItems,
-      amount: Number(req.body.amount),
-      subtotal: Number(req.body.subtotal),
-      discount: Number(req.body.discount),
-      tax: Number(req.body.tax),
-      shipping: Number(req.body.shipping),
-      total: Number(req.body.total),
-      amount_paid: Number(req.body.amount_paid),
-      notes: req.body.notes,
-      terms: req.body.terms
-    });
-    
-    User.findOne({auth0_userID}).then(user => {
-      newInvoice.save().then(invoice => {
-          user.invoices.push(invoice._id);
-          user.save().then(() => {
-            res.send("Success!");
+  cloudinary.v2.uploader.upload(
+    tmp,
+    { public_id: `auto-invoicer/${Date.now()}` },
+    (error, result) => {
+      const newInvoice = new Invoice({
+        logo: result.secure_url,
+        invoice_number: Number(req.body.invoice_number),
+        date: req.body.date,
+        due_date: req.body.due_date,
+        balance_due: Number(req.body.balance_due),
+        address: req.body.address,
+        zipcode: req.body.zipcode,
+        city: req.body.city,
+        state: req.body.state,
+        company_name: req.body.company_name,
+        // item: req.body.item,
+        // quantity: Number(req.body.quantity),
+        // rate: Number(req.body.rate),
+        invoiceTo: req.body.invoiceTo,
+        line_items: lineItems,
+        amount: Number(req.body.amount),
+        subtotal: Number(req.body.subtotal),
+        discount: Number(req.body.discount),
+        tax: Number(req.body.tax),
+        shipping: Number(req.body.shipping),
+        total: Number(req.body.total),
+        amount_paid: Number(req.body.amount_paid),
+        notes: req.body.notes,
+        terms: req.body.terms
+      });
+
+      User.findOne({ auth0_userID }).then(user => {
+        newInvoice
+          .save()
+          .then(invoice => {
+            user.invoices.push(invoice._id);
+            user.save().then(() => {
+              res.send("Success!");
+            });
           })
-      }).catch(err => console.log(err))
-    });
-  })
-
+          .catch(err => console.log(err));
+      });
+    }
+  );
 });
 
-router.put("/api/invoices/:_id", upload.single('logo'), (req, res) => {
+router.put("/api/invoices/:_id", upload.single("logo"), (req, res) => {
   // for editing existing invoices
   let options = {
-      new: true
-    }
+    new: true
+  };
 
   const invoice_number = req.params._id;
   let single_invoice;
@@ -142,23 +149,77 @@ router.put("/api/invoices/:_id", upload.single('logo'), (req, res) => {
   lineItems.forEach(row => {
     row.quantity = Number(row.quantity);
     row.rate = Number(row.rate);
-  })
-  
-  if(req.file){
-  const ext = req.file.originalname.split('.')[1]
-  const tmp = os.tmpdir() + '/deleteme.' + ext
-  fs.writeFileSync(tmp, req.file.buffer)
-  
+  });
 
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
-  })
+  if (req.file) {
+    const ext = req.file.originalname.split(".")[1];
+    const tmp = os.tmpdir() + "/deleteme." + ext;
+    fs.writeFileSync(tmp, req.file.buffer);
 
-  cloudinary.v2.uploader.upload(tmp, {public_id: `auto-invoicer/${Date.now()}`}, (error, result) => {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET
+    });
+
+    cloudinary.v2.uploader.upload(
+      tmp,
+      { public_id: `auto-invoicer/${Date.now()}` },
+      (error, result) => {
+        const edit = {
+          logo: result.secure_url,
+          invoice_number: Number(req.body.invoice_number),
+          date: req.body.date,
+          due_date: req.body.due_date,
+          balance_due: Number(req.body.balance_due),
+          address: req.body.address,
+          zipcode: req.body.zipcode,
+          city: req.body.city,
+          state: req.body.state,
+          company_name: req.body.company_name,
+          invoiceTo: req.body.invoiceTo,
+          line_items: lineItems,
+          // item: req.body.item,
+          // quantity: Number(req.body.quantity),
+          // rate: Number(req.body.rate),
+          amount: Number(req.body.amount),
+          subtotal: Number(req.body.subtotal),
+          discount: Number(req.body.discount),
+          tax: Number(req.body.tax),
+          shipping: Number(req.body.shipping),
+          total: Number(req.body.total),
+          amount_paid: Number(req.body.amount_paid),
+          notes: req.body.notes,
+          terms: req.body.terms
+        };
+
+        User.findOne({ auth0_userID })
+          .populate("invoices")
+          .then(user => {
+            user.invoices.forEach(invoice => {
+              if (invoice._id == invoice_number) single_invoice = invoice._id;
+            });
+
+            Invoice.updateOne({ _id: single_invoice }, edit)
+              .then(invoice => {
+                res.status(200).send(invoice);
+              })
+              .catch(err => {
+                res.status(500);
+                console.log(
+                  err,
+                  "User look up successful but unable to find invoice with that ID"
+                );
+              });
+          })
+          .catch(err => {
+            res.status(500);
+            console.log(err, "Unable to look up user for invoice ID.");
+          });
+      }
+    );
+  } else {
     const edit = {
-      logo: result.secure_url,
       invoice_number: Number(req.body.invoice_number),
       date: req.body.date,
       due_date: req.body.due_date,
@@ -183,94 +244,42 @@ router.put("/api/invoices/:_id", upload.single('logo'), (req, res) => {
       notes: req.body.notes,
       terms: req.body.terms
     };
-  
 
-
-  User.findOne({auth0_userID})
-      .populate('invoices')
+    User.findOne({ auth0_userID })
+      .populate("invoices")
       .then(user => {
-        user.invoices.forEach( invoice => {
-          if(invoice._id == invoice_number)
-            single_invoice = invoice._id;
-        })
+        user.invoices.forEach(invoice => {
+          if (invoice._id == invoice_number) single_invoice = invoice._id;
+        });
 
-        Invoice.updateOne({ _id: single_invoice}, edit)
-          .then(invoice =>{
+        Invoice.updateOne({ _id: single_invoice }, edit)
+          .then(invoice => {
             res.status(200).send(invoice);
-        })
-          .catch(err =>{
-            res.status(500);
-            console.log(err, "User look up successful but unable to find invoice with that ID");
           })
+          .catch(err => {
+            res.status(500);
+            console.log(
+              err,
+              "User look up successful but unable to find invoice with that ID"
+            );
+          });
       })
       .catch(err => {
         res.status(500);
         console.log(err, "Unable to look up user for invoice ID.");
-      })
-    })
-  }
-
-  else{
-    const edit = {
-      invoice_number: Number(req.body.invoice_number),
-      date: req.body.date,
-      due_date: req.body.due_date,
-      balance_due: Number(req.body.balance_due),
-      address: req.body.address,
-      zipcode: req.body.zipcode,
-      city: req.body.city,
-      state: req.body.state,
-      company_name: req.body.company_name,
-      invoiceTo: req.body.invoiceTo,
-      line_items: lineItems,
-      // item: req.body.item,
-      // quantity: Number(req.body.quantity),
-      // rate: Number(req.body.rate),
-      amount: Number(req.body.amount),
-      subtotal: Number(req.body.subtotal),
-      discount: Number(req.body.discount),
-      tax: Number(req.body.tax),
-      shipping: Number(req.body.shipping),
-      total: Number(req.body.total),
-      amount_paid: Number(req.body.amount_paid),
-      notes: req.body.notes,
-      terms: req.body.terms
-    }
-
-    User.findOne({auth0_userID})
-      .populate('invoices')
-      .then(user => {
-        user.invoices.forEach( invoice => {
-          if(invoice._id == invoice_number)
-            single_invoice = invoice._id;
-        })
-
-        Invoice.updateOne({ _id: single_invoice}, edit)
-          .then(invoice =>{
-            res.status(200).send(invoice);
-        })
-          .catch(err =>{
-            res.status(500);
-            console.log(err, "User look up successful but unable to find invoice with that ID");
-          })
-      })
-      .catch(err => {
-        res.status(500);
-        console.log(err, "Unable to look up user for invoice ID.");
-      })
+      });
   }
 });
 
 router.delete("/:_id", (req, res) => {
-  Invoice.findOneAndRemove({ _id: req.params._id})
-      .then(item => {
-        res.send(item)
-      })
-      .catch(err=>{
-        res.status(500)
-        console.log(err)
-      })
-})
-
+  Invoice.findOneAndRemove({ _id: req.params._id })
+    .then(item => {
+      res.send(item);
+    })
+    .catch(err => {
+      res.status(500);
+      console.log(err);
+    });
+});
 
 module.exports = router;
