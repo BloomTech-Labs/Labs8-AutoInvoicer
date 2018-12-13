f8d7076045a9d5a96437d127708a5c083c4ec6af
markdavidwilliams <markwilliams.aus@gmail.com>
2018-12-13 17:22:05 -0600

    server clean up abstracted everything in server to config files removed cors

--- config/modules.js
+++ config/modules.js
@@ -1,12 +1,22 @@
-const express = require('express')
+const expressjson = require("express").json();
+const morgan = require("morgan");
 const helmet = require("helmet");
-// const cors = require("cors");
-const bodyParser = require("body-parser");
+const flash = require("connect-flash");
 
 module.exports = server => {
   server.use(helmet());
-  // server.use(cors());
-  // server.use(bodyParser.urlencoded({ extended: false }));
-  // server.use(bodyParser.json());
-  // server.use(express.json())
+  server.use(morgan("combined"));
+  server.use(expressjson);
+  server.use(flash());
+
+  // Handle auth failure error messages
+  server.use(function(req, res, next) {
+    if (req && req.query && req.query.error) {
+      req.flash("error", req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+      req.flash("error_description", req.query.error_description);
+    }
+    next();
+  });
 };
--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const { server } = require("./server");
-
-// TODO
-// import connectMongo here
-// restructure server.listen
-
-// with a proper mongo connection, the rest of the schemas will work
-
-require("dotenv").config();
+const server = require("./server");
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`API running on Port ${PORT}`));
